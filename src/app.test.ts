43d42dc4c721db17f29a5b2a47fc5615f225a2d3
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-24 19:05:07 +0530

    test: setup mongodb-memory-server

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,30 @@
 import supertest from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import createServer from "./utils/server";
 
-const request = supertest("localhost:3000");
+const app = createServer();
+const request = supertest(app);
+let mongoServer: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
 
 test("GET /users", async () => {
   const response = await request.get("/users");
