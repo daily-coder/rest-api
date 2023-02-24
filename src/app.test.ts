bb623f752bab2bae9c60f111aec59f8925812192
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-24 19:05:07 +0530

    fix(test): 'index' route returns all users
    By default database has no users which fails the test. To make test pass
    a user is added to the database.

--- src/app.test.ts
+++ src/app.test.ts
@@ -2,10 +2,20 @@ import supertest from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import createServer from "./utils/server";
+import { build } from "@jackfranklin/test-data-bot";
+import { faker } from "@faker-js/faker";
+import User from "./models/user";
 
 const app = createServer();
 const request = supertest(app);
 let mongoServer: MongoMemoryServer;
+const userBuilder = build({
+  fields: {
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  },
+});
 
 beforeAll(async () => {
   mongoServer = await MongoMemoryServer.create();
@@ -27,7 +37,10 @@ afterAll(async () => {
 });
 
 test("GET /users", async () => {
+  const newUser = new User(userBuilder());
+  await newUser.save();
+
   const response = await request.get("/users");
-  expect(response.body).toHaveLength(2);
+  expect(response.body.length).toBeGreaterThan(0);
   expect(response.status).toBe(200);
 });
