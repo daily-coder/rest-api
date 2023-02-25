3655f2e25f3682ebeee8278e0020b8645e5e24d3
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-25 19:03:04 +0530

    feat: setup morgan logging middleware

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 import userRoutes from "../routes/users";
 
 function createServer() {
   const app = express();
+  app.use(morgan("common"));
   app.use(express.urlencoded({ extended: true }));
   app.use("/users", userRoutes);
   return app;
