b9e9f7f01872afd2ad63db3e4127e858f0ed795c
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-25 19:03:04 +0530

    test: GET /users/:id route

--- src/app.test.ts
+++ src/app.test.ts
@@ -44,3 +44,13 @@ test("GET /users", async () => {
   expect(response.body.length).toBeGreaterThan(0);
   expect(response.status).toBe(200);
 });
+
+test("GET /users/:id", async () => {
+  const userInfo = userBuilder();
+  const newUser = new User(userInfo);
+  await newUser.save();
+
+  const response = await request.get(`/users/${newUser._id}`);
+  expect(response.status).toBe(200);
+  expect(response.body).toMatchObject(userInfo);
+});
