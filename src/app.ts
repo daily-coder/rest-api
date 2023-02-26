0f04126c9840bf029e34a7cb6b070a6355abbe25
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-26 20:04:08 +0530

    feat: setup express with dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import connect from "./utils/connect";
 import createServer from "./utils/server";
 
 const app = createServer();
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
   connect();
 });
