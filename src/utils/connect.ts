bbd7bb920708c3c62702e1a4fffaec3c706120f3
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-26 20:04:08 +0530

    feat: setup express with dotenv

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,8 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
+
+const port = process.env.MONGODB_URL || "mongodb://localhost:27017/demoApp";
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/demoApp");
+    await mongoose.connect(port);
     console.log("MONGODB CONNECTED");
   } catch (error) {
     console.log("MONGODB CONNECTION ERROR: ", error);
