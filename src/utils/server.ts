267f5b35071c2db647af0dbd7a14c41328d45fcd
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-28 20:56:22 +0530

    feat: create GET route to get all flashcards

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import userRoutes from "../routes/users";
+import flashcardRoutes from "../routes/flashcards";
 
 function createServer() {
   const app = express();
   app.use(morgan("common"));
   app.use(express.urlencoded({ extended: true }));
   app.use("/users", userRoutes);
+  app.use("/flashcards", flashcardRoutes);
   return app;
 }
 
