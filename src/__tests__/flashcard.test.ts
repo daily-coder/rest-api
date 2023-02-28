61a1925179b70ffe9eafbc1cdf1f6ebfe14c976f
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-28 20:56:22 +0530

    test: GET route to get all flashcards

---
+++ src/__tests__/flashcard.test.ts
@@ -0,0 +1,49 @@
+import supertest from "supertest";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import createServer from "../utils/server";
+import { build } from "@jackfranklin/test-data-bot";
+import { faker } from "@faker-js/faker";
+import Flashcard from "../models/flashcard";
+
+const app = createServer();
+const request = supertest(app);
+let mongoServer: MongoMemoryServer;
+const flashcardBuilder = build({
+  fields: {
+    question: faker.lorem.paragraph(),
+    answer: faker.lorem.paragraph(),
+  },
+});
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterEach(async () => {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    const collection = collections[key];
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
+
+test("GET | return all flashcards from database", async () => {
+  const flashcardInfo = flashcardBuilder();
+  const newFlashcard = new Flashcard(flashcardInfo);
+  await newFlashcard.save();
+
+  const response = await request.get("/flashcards");
+  expect(response.status).toBe(200);
+  expect(response.body.length).toBeGreaterThan(0);
+  // newFlashcard contains additional methods like save, so don't use it to compare
+  // returned flashcard object in response.body
+  expect(response.body).toContainEqual(expect.objectContaining(flashcardInfo));
+});
