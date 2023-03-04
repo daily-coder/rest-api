355a0661c9a1bee45370ed02cc7881a06e2ec7f8
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-03-04 20:41:05 +0530

    feat: setup express-session

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -3,9 +3,27 @@ import morgan from "morgan";
 import userRoutes from "../routes/users";
 import flashcardRoutes from "../routes/flashcards";
 import errorHandler from "../middleware/errorHandler";
+import session from "express-session";
+
+const sessionKey = process.env.SESSION_SECRET;
+
+if (!sessionKey) {
+  throw new Error("session key is required");
+}
+
+const sessionConfig = {
+  secret: sessionKey,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
 
 function createServer() {
   const app = express();
+  app.use(session(sessionConfig));
   app.use(morgan("dev"));
   app.use(express.urlencoded({ extended: true }));
   app.use("/users", userRoutes);
