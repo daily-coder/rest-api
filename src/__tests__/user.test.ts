49158727dd8bd3cb7139b255820e2d9b49ed2cc1
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-03-04 20:41:05 +0530

    refactor: cleanup user routes and tests

--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -13,22 +13,3 @@ const userBuilder = build({
     password: faker.internet.password(),
   },
 });
-
-test("GET /users", async () => {
-  const newUser = new User(userBuilder());
-  await newUser.save();
-
-  const response = await request.get("/users");
-  expect(response.body.length).toBeGreaterThan(0);
-  expect(response.status).toBe(200);
-});
-
-test("GET /users/:id", async () => {
-  const userInfo = userBuilder();
-  const newUser = new User(userInfo);
-  await newUser.save();
-
-  const response = await request.get(`/users/${newUser._id}`);
-  expect(response.status).toBe(200);
-  expect(response.body).toMatchObject(userInfo);
-});
