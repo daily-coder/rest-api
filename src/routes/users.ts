ce55ceb9983ab5166a03010cc8ea2a0bc750e90f
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-03-05 18:49:24 +0530

    feat: create POST route to register new user

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,14 +3,26 @@ import User from "../models/user";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
-});
+function getErrorMessage(error: unknown) {
+  if (error instanceof Error) return error.message;
+  else return String(error);
+}
 
-router.get("/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
-  res.send(user);
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = new User({ username, email });
+    const registeredUser = await User.register(user, password);
+    req.logIn(registeredUser, (error) => {
+      if (error) {
+        next(error);
+      } else {
+        res.send({ message: `successfully registered user: ${username}` });
+      }
+    });
+  } catch (error) {
+    res.status(404).send({ message: getErrorMessage(error) });
+  }
 });
 
 export default router;
