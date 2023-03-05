2b9310217f32fe284229cd0ac958dbb89489cfa1
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-03-05 18:49:24 +0530

    feat: setup passport

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import morgan from "morgan";
 import userRoutes from "../routes/users";
 import flashcardRoutes from "../routes/flashcards";
 import errorHandler from "../middleware/errorHandler";
 import session from "express-session";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "../models/user";
 
 const sessionKey = process.env.SESSION_SECRET;
 
@@ -24,9 +29,15 @@ const sessionConfig = {
 function createServer() {
   const app = express();
   app.use(session(sessionConfig));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+
   app.use(morgan("dev"));
   app.use(express.urlencoded({ extended: true }));
-  app.use("/users", userRoutes);
+  app.use("/", userRoutes);
   app.use("/flashcards", flashcardRoutes);
   app.all("*", (req, res) => {
     res.status(404).send({ message: "Not Found" });
